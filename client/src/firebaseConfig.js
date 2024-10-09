b11d23e79d511296d02e51f191bf5b1f443a5692
syedalthaf786 <syedalthaf760.com>
2024-10-09 13:02:02 +0530

    secure added for firebase

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -3,15 +3,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB1uryO5tGqQtjOTQvDK-whVzjf7Phbjkc",
-    authDomain: "learnbydoing-328df.firebaseapp.com",
-    projectId: "learnbydoing-328df",
-    storageBucket: "learnbydoing-328df.appspot.com",
-    messagingSenderId: "125551380359",
-    appId: "1:125551380359:web:e98c3dba0bf21e1c173522"
-  };
 // Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
